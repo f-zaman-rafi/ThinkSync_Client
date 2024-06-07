dbefd06e1f40d2c704b82bbcbc7f391b71a9058f
F-Zaman-Rafi <gollarafi@gmail.com>
2024-06-08 02:43:49 +0800

    added study session

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -9,7 +9,6 @@ const Banner = () => {
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
                     <h1 className="mb-5 text-5xl font-bold">WELCOME there</h1>
-
                 </div>
             </div>
         </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "./Banner/Banner";
+import StudySessionSection from "./StudySession/StudySessionSection";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <StudySessionSection></StudySessionSection>
         </div>
     );
 };
