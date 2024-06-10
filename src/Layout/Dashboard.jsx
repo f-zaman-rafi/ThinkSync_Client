179c31c98ab2ce52949f0b466aa975f75d387cbd
F-Zaman-Rafi <gollarafi@gmail.com>
2024-06-10 15:48:52 +0800

    dashboard structure ready

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,17 +1,32 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import DashNav from "../Pages/Dashboard/DashNav/DashNav";
 
 const Dashboard = () => {
     return (
         <div>
-            <div>
-                <h1>
-                    hit thehe
-                    <Link to='all-users'><p className="btn btn-warning">All users</p></Link>
-                </h1>
-            </div>
+            <DashNav></DashNav>
             <Outlet></Outlet>
         </div>
     );
 };
 
 export default Dashboard;
+
+
+
+// .The tutor dashboard has the following route:
+// a.Create study session
+// b.View all study sessions created by a tutor
+// c.Upload materials
+// d.View all materials
+
+// Student dashboard has the following route:
+// a.View booked session
+// b.Create note
+// c.Manage personal notes
+// d.View all study materials provided by the tutor\
+
+// 7. Admin dashboard has the following route:
+// a.View all users
+// b.View all study session
+// c.View all materials
