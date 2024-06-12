ff3445e0afbeb7e2b0345f9d18d851286aa9f4f2
F-Zaman-Rafi <gollarafi@gmail.com>
2024-06-13 02:53:35 +0800

    working on edit a session by admin

--- src/Pages/Dashboard/AllSessionsDash/AllSessionsDash.jsx
+++ src/Pages/Dashboard/AllSessionsDash/AllSessionsDash.jsx
@@ -52,7 +52,7 @@ const AllSessionsDash = () => {
             });
     };
 
-
+    const filteredSessions = sessionData.filter(session => session.Status !== 'Rejected');
 
     // Session rejection
     const handleSessionReject = session => {
@@ -139,8 +139,9 @@ const AllSessionsDash = () => {
                                         {/* ..........................................Distruct here................................... */}
 
 
+
                                         {
-                                            sessionData.map(session => (
+                                            filteredSessions.map(session => (
                                                 <tbody key={session._id} className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
                                                     <tr>
                                                         <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
@@ -169,9 +170,9 @@ const AllSessionsDash = () => {
                                                             <div className="flex items-center gap-x-6">
                                                                 {/* Edit Button */}
                                                                 <div className="flex justify-center">
-                                                                    <button onClick={() => { document.getElementById('my_modal_1').showModal(); window.currentUser = session; }} className="text-gray-500 transition-colors duration-200 dark:hover:text-yellow-500 dark:text-gray-300 hover:text-yellow-500 focus:outline-none">
-                                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
-                                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                                                    <button onClick={() => { document.getElementById('my_modal_1').showModal(); window.currentUser = session; }} className="text-gray-500 transition-colors duration-200 dark:hover:text-green-500 dark:text-gray-300 hover:text-green-500 focus:outline-none">
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-5 h-5">
+                                                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
                                                                         </svg>
                                                                     </button>
                                                                     <dialog id="my_modal_1" className="modal">
@@ -272,19 +273,25 @@ const AllSessionsDash = () => {
                                                                     </div>
                                                                 </dialog>
 
-                                                            </div>
+                                                                {/* Delete Button */}
 
-
-                                                            {/* Delete Button */}
-
-                                                            {/* <button onClick={() => handleDeleteUser(session)} className="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none">
+                                                                <button onClick={() => handleDeleteUser(session)} className="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                                                    </svg>
+                                                                </button>
+                                                                <button onClick={() => handleDeleteUser(session)} className="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none">
                                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                                     </svg>
-                                                                </button> */}
+                                                                </button>
+                                                            </div>
 
 
-                                                            {/* </div> */}
+
+
+
+
                                                         </td>
                                                     </tr>
 
